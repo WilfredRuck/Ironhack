a96e503c837cbec7a5c83185d6617a99c0086645
Wilfred Ruck <wilfredoruck@gmail.com>
2016-08-28 19:39:46 -0400

    organizing

--- Week2/imdb_game/app.rb
+++ Week2/imdb_game/app.rb
@@ -7,12 +7,11 @@ get "/" do
 end
 
 post "/imdb_search" do
-	@movie_string = params[:text]
-	the_search = Imdb::Search.new(@movie_string)
+	the_search = Imdb::Search.new(params[:text])
 	@each_movie = the_search.movies.take(20)
 	@each_movie.delete_if { |the_movie| the_movie.poster == nil}
 	@movies = @each_movie.take(9)
-
+	@the_chosen_one = @movies.sample(1)
 	erb :results
 end
 
