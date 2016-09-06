016660aeecf5e4eb2ca8b50e974bcf30ee2bf59a
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-05 20:10:47 -0400

    updating

--- Week3/Quiz/app.js
+++ Week3/Quiz/app.js
@@ -2,28 +2,50 @@
 
 
 class Question {
-	constructor(question){
-		this.question = question;
+	constructor(){
+		// this.answer1 = answer1;
+		// this.answer2 = answer2;
+		// this.answer3 = answer3;
+		// this.id = id;
+	}
+
+	// firstQuestion{
+		
+	// }
+
+	// secondQuestion{
+
+	// }
+	
+	// thirdQuestion{
+
+	// }
+	
+}
+
+class Quiz {
+	constructor(a_answer){
+		this.a_answer = a_answer;
 	}
 
 	 quiz(){
+	 	if (this.a_answer)
 		console.log("\nAnswer: Don't ask stupid questions.");
 	}
-	
 }
 
 var read = require('read');
 
 var options = {
-    prompt: "What's your question?\n"
+    prompt: "What's your answer?  "
 } // Our options object, the prompt is simply what will appear in the command line when read is called
 
 read(options, displayName);
 // The prompt itself, passing options, and using our callback function after input
 
-function displayName (err, firstQuestion){
-   var question1 = new Question(firstQuestion);
-   question1.quiz(); 
+function displayName (err, answers){
+   var answer = new Quiz(answers);
+   answer.quiz(); 
 }
 // Outputs whatever the user has entered back to the console
 
