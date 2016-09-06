c36ee4ef7a17d3ae3d00462bdced5782db9b1bec
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-06 12:13:52 -0400

    testing rails

--- Week4/timetracking/config/routes.rb
+++ Week4/timetracking/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 	get '/', to: 'site#home'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 	get '/contact', to: 'site#contact'
+
+	get '/say_name/:name', to: 'site#say_name'
 end
