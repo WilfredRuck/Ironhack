633efcf6ced44a91ee203d8e53aaef531582c3e3
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-05 21:02:05 -0400

    updating

--- Week3/Quiz/app.js
+++ Week3/Quiz/app.js
@@ -2,24 +2,32 @@
 
 
 class Question {
-	constructor(){
-		// this.answer1 = answer1;
-		// this.answer2 = answer2;
-		// this.answer3 = answer3;
-		// this.id = id;
+	constructor(idcheck){
+		this.idcheck = idcheck;
 	}
 
-	// firstQuestion{
+	firstQuestion(){
+		var id = 1;
 		
-	// }
+		if (this.idcheck === id){
+			var question1 = "What is the most popular fruit in the US? ";
+			var answer1 = "apples";
+			console.log("You got this part to work!");
+			console.log(answer1);
+		}
+	}
 
-	// secondQuestion{
-
-	// }
+	secondQuestion(){
+		var id = 2;
+		var question2 = "What is the best primary color? ";
+		var answer2 = "red";
+	}
 	
-	// thirdQuestion{
-
-	// }
+	thirdQuestion(){
+		var id = 3;
+		var question3 = "What is the most popular fruit in the US? ";
+		var answer3 = apples;
+	}
 	
 }
 
@@ -34,20 +42,32 @@ class Quiz {
 	}
 }
 
-var read = require('read');
 
-var options = {
-    prompt: "What's your answer?  "
-} // Our options object, the prompt is simply what will appear in the command line when read is called
 
-read(options, displayName);
-// The prompt itself, passing options, and using our callback function after input
-
-function displayName (err, answers){
-   var answer = new Quiz(answers);
-   answer.quiz(); 
+function whichQuestion() {
+	var id = 1;
+	var thisId = new Question(id);
+	thisId.firstQuestion();
 }
-// Outputs whatever the user has entered back to the console
+
+whichQuestion();
+
+// var read = require('read');
+
+// var options = {
+//     prompt: "What's your answer?  "
+// } // Our options object, the prompt is simply what will appear in the command line when read is called
+
+// read(options, giveAnswer);
+// // The prompt itself, passing options, and using our callback function after input
+
+
+
+// function giveAnswer (err, answers){
+//    var answer = new Quiz(answers);
+//    answer.quiz(); 
+// }
+// // Outputs whatever the user has entered back to the console
 
 
 
