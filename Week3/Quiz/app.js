adaee662ff61f2b580e046e1d46bc3f3c677c309
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-06 09:31:04 -0400

    edited comments

--- Week3/Quiz/app.js
+++ Week3/Quiz/app.js
@@ -22,13 +22,19 @@ class Question {
 		if (this.idcheck === id)
 			var question2 = "What is the best primary color? ";
 			var answer2 = "red";
-			console.log("YOU MADE IT TO THE SECOND QUESTION! YESSS!");
+			// console.log("YOU MADE IT TO THE SECOND QUESTION! YESSS!");
+			// console.log(question2);
 	}
 	
 	thirdQuestion(){
 		var id = 3;
-		var question3 = "What is the most popular fruit in the US? ";
-		var answer3 = apples;
+
+		if (this.idcheck === id){
+			var question3 = "What is the best borough in New York City? ";
+			var answer3 = "Queens";
+			// console.log("You printed all the questions. You the man.");
+			// console.log(question3);
+		}
 	}
 	
 }
@@ -40,18 +46,23 @@ class Quiz {
 
 	 quiz(){
 	 	this.a_answer[0].toUpperCase();
-	 	if (this.a_answer !== "Apples"){
+	 	if (this.a_answer !== "Apples"){ //create a global variable instead of inputing a value
 			console.log("\nResponse: Nah b.");
 			console.log("Answer: Apples");
 			console.log("\n Try Again \n");
-			whichQuestion();
+			whichQuestion();			//create something to figure out which Question we are up to so it can
+										//repeat that same question
 			read(options,giveAnswer);
 		}
 
-		else{
+		else{ 		//create a variable that counts up a number everytime 
+			  		//a right answer is given so that it can call the next question!
+			  		//ex. var countQuestion = 1; 
+			  		//    countQuestion += 1;
+			  		//    whichQuestion(countQuestion);
 			console.log("\nResponse: Yassssss!");
 			console.log("\n\n Next Question! Get Ready. \n\n");
-			// whichQuestion(id + 1);
+			// whichQuestion(id + 1);  <<-- don't have access to "id", can't use this
 			// read(options,giveAnswer);
 			console.log("Sike, haven't programmed this part yet\n");
 		}
