d661128d206f57e49f52a49acdffe6026d0c7ccd
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-07 13:42:28 -0400

    created 10 Project seeds

--- Week4/timetracking/db/seeds.rb
+++ Week4/timetracking/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.create!(name: 'Ironhack', description: 'Ironhack is a....')
+
+Project.create!(name: 'Time Tracking App', description: 'Project finished')
+
+Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
+
+Project.create!(name: 'Pancakes', description: 'Chocolate Chip')
+
+Project.create!(name: 'Miami', description: 'Hot version of New York with crazy drivers')
+
+Project.create!(name: 'MacBook', description: 'A laptop')
+
+Project.create!(name: 'Lamborghini', description: 'A fast car')
+
+Project.create!(name: 'Henessy Venom GT', description: 'Thee Fastest Car')
+
+Project.create!(name: 'Red', description: 'The best color in the world')
+
+Project.create!(name: 'Delta', description: 'An airline')
