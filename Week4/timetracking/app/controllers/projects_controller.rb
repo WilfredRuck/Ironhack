ffebe29ef987c209752acc3664836bcaa288510f
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-08 10:03:22 -0400

    added id page

--- Week4/timetracking/app/controllers/projects_controller.rb
+++ Week4/timetracking/app/controllers/projects_controller.rb
@@ -3,6 +3,11 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.order(created_at: :DESC).limit(10)
 
-		render "index"
+		render 'index'
+	end
+
+	def show
+		@project = Project.find_by(id: params[:id])
+		render 'show'
 	end
 end
