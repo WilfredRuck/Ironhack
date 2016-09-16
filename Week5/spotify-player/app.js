c9694bbe263e5dc4eeac4f5a73bbdc11bc1b7089
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-16 17:57:26 -0400

    Added Working Progress Bar

--- Week5/spotify-player/app.js
+++ Week5/spotify-player/app.js
@@ -3,83 +3,90 @@ $(document).ready(function(){
 
 	$('.btn-play').on('click', previewSong);
 
-})
+
 
 
 
 //===========================================================================
 
-function findSong (theEvent){
-	theEvent.preventDefault();
+	function findSong (theEvent){
+		theEvent.preventDefault();
 
-	var song = $(".js-song").val();
-	console.log(song);
+		var song = $(".js-song").val();
+		console.log(song);
 
-	$.ajax({
-		type: "GET",
-		url: `https://api.spotify.com/v1/search?type=track&query=${song}`,
-		success: showSongs,
-		error: handleError
-	});
+		$.ajax({
+			type: "GET",
+			url: `https://api.spotify.com/v1/search?type=track&query=${song}`,
+			success: showSongs,
+			error: handleError
+		});
 
-	// open(`https://api.spotify.com/v1/tracks/${song.id}`);
+		// open(`https://api.spotify.com/v1/tracks/${song.id}`);
 
 
-	function showSongs(response){
-		$('.js-song-info').empty();
-		$('.js-song-album-cover').empty();
-		$('.js-song-preview').empty();
-		var firstSong = response.tracks.items[0];
-		console.log(response.tracks.items[0]);
+		function showSongs(response){
+			$('.js-song-info').empty();
+			$('.js-song-album-cover').empty();
+			$('.js-song-preview').empty();
+			var firstSong = response.tracks.items[0];
+			console.log(response.tracks.items[0]);
 
-		var songDetails = `<p>
-			 			<b> ${firstSong.name} </b>
-			 			<br>
-			 			${firstSong.album.name} 
-			 		</p>
-		`;
-		$('.js-song-info').append(songDetails);
+			var songDetails = `<p>
+				 			<b> ${firstSong.name} </b>
+				 			<br>
+				 			${firstSong.album.name} 
+				 		</p>
+			`;
+			$('.js-song-info').append(songDetails);
+			
+			console.log(firstSong.name);
+			console.log(firstSong.album.name);
+
+			var songAlbumCover = `
+				<img src=${firstSong.album.images[1].url}>
+			`;
+			$('.js-song-album-cover').append(songAlbumCover);	
 		
-		console.log(firstSong.name);
-		console.log(firstSong.album.name);
+			var songPreview =`
+				<audio src="${firstSong.preview_url}" class="js-audio"> </audio>
+			`;
+			
+			$('.js-song-preview').append(songPreview);
+			
+		}
 
-		var songAlbumCover = `
-			<img src=${firstSong.album.images[1].url}>
-		`;
-		$('.js-song-album-cover').append(songAlbumCover);	
-	
-		var songPreview =`
-			<audio src="${firstSong.preview_url}" class = "js-player"> </audio>
-		`;
-		$('.js-song-preview').append(songPreview);
-		
-	}
+		function handleError(error){
+			console.log("thumbs down");
+			console.log(error.responseText);
 
-	function handleError(error){
-		console.log("thumbs down");
-		console.log(error.responseText);
-
-	}
-	
-}
-
-function previewSong(){
-	$('.btn-play').toggleClass('playing');
-	
-	if ( $('.btn-play').hasClass('playing') ) {
-		$('.js-player').trigger('play');
-		
+		}
 		
 	}
-	else {
-		$('.js-player').trigger('pause');
-	}
-	
-	
 
+	function previewSong(){
+		$('.btn-play').toggleClass('playing');
 		
+		if ( $('.btn-play').hasClass('playing') ) {
+			$('.js-audio').trigger('play');
+			$('.js-audio').on('timeupdate', printTime);
 
-}
+		}
+		else {
+			$('.js-audio').trigger('pause');
+		}
+		
+	};
+
+	function printTime(){
+	  var current = $('.js-audio').prop('currentTime');
+	  
+		console.log('Current time: ' + current);
+	  $('.time').prop('value', current);
+
+	};
+})
+
 
 
 
