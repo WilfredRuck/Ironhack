22711dcb7f2b2d965b6c423668d6775172b48f19
Wilfred Ruck <wilfredoruck@gmail.com>
2016-09-21 16:17:37 -0400

    finished

--- week6/sandwichr/config/routes.rb
+++ week6/sandwichr/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
 resources :sandwiches, only: [:index, :show], controller: "sandwich_views"	
 	scope "/api" do
   		resources :ingredients
   		resources :sandwiches
   		post '/sandwiches/:id/ingredients/add', to: 'sandwiches#add'
 	end
+
+	root 'users#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
