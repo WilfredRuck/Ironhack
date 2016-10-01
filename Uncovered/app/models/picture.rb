8673c1d1cba0d145ba9cb8be3acf784d65f436f9
Wilfred Ruck <wilfredoruck@gmail.com>
2016-10-01 17:53:56 -0400

    changed gem file to add exif & exifr

--- Uncovered/app/models/picture.rb
+++ Uncovered/app/models/picture.rb
@@ -1,9 +1,10 @@
 class Picture < ApplicationRecord
-  belongs_to :user
-  has_attached_file :image, styles: { large: "300x300" }
+	belongs_to :user
+	has_attached_file :image, styles: { large: "500x500" }
 
-  validates_attachment_content_type :image, :content_type => ['image/jpeg']
+	validates_attachment_content_type :image, :content_type => ['image/jpeg']
+	
+	validates :caption, :image_file_name, presence:  :true
+	validates_length_of :caption, :maximum => 140
 
-  validates :caption, :image_file_name, presence:  :true
-  validates_length_of :caption, :maximum => 140
 end
