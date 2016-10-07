cb74e075aad421f11a2b6291396836155ad1f8d4
Wilfred Ruck <wilfredoruck@gmail.com>
2016-10-06 20:13:13 -0400

    changed backgrounds

--- Uncovered/app/assets/javascripts/like.js
+++ Uncovered/app/assets/javascripts/like.js
@@ -1,7 +1,5 @@
-
 $(document).ready(function() {
 
-  console.log('DA FOOK')
   $(".js-update-number").on("ajax:success", function(e, response) { 
     $(`.js-count-${response.picture_id}`).html(response.upvotes);
   });
