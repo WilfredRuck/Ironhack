6a26626434d9564c14e8462ce5e690d91dc79803
Wilfred Ruck <wilfredoruck@gmail.com>
2016-10-06 20:51:35 -0400

    changed name of everything from Uncovered to Photo-Graphed, because there wasn't an available .co

--- Uncovered/config/environments/production.rb
+++ Uncovered/config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = {:host => "uncovered.herokuapp.com"}
+  config.action_mailer.default_url_options = {:host => "photo-graphed.co"}
 
 
   # Eager load code on boot. This eager loads most of Rails and
