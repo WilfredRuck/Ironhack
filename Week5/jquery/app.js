3a86cd0aef4b63291011a303c25a46b8736d1f85
Wilfred Ruck <wilfredoruck@gmail.com>
2017-02-24 15:22:18 -0500

    updates

--- Week5/jquery/app.js
+++ Week5/jquery/app.js
@@ -1,27 +1,10 @@
-$ (document).on('ready', function(){
+$(document).on('ready', function(){
  
 
-
-	$(".js-like-button").on('click', function (){
-		if ( $(".js-like-button").hasClass('btn-success') ){
-			$(".js-like-button").html("Like");
-		}
-
-		else {
-			$(".js-like-button").html("Liked +1");
-		}
-
-		$(".js-like-button").toggleClass('btn-default')
-		$(".js-like-button").toggleClass('btn-success')	
-	});
-
-	$(".unicorn-mode").on("click", function(){
-		$("").style.color();
-	});
-
-
-	$(".except-me").on("click", function(){
-		$("div").fadeToggle(200);
+	$(".fade").on("click", function(){
+		
+		$("div").fadeToggle(0);
+	
 	})
 
 });
